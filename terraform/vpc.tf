3e4884dad9195e8fa89601f947305f9382f56305
Barry Botha <barry@hydracorp.ltd>
2023-05-18 16:34:30 +0300

    auto subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
 resource "google_compute_network" "zenotta-mining-network" {
   name                    = "zenotta-mining-network"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = true
 }
