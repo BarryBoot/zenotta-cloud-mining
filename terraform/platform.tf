675883b8dee191fcb25f0fff2e8902c84d88226d
Barry Botha <barry@hydracorp.ltd>
2023-05-18 16:50:42 +0300

    test with no iam

--- terraform/platform.tf
+++ terraform/platform.tf
@@ -4,22 +4,22 @@ resource "google_project" "zenotta-mining" {
   auto_create_network = false
 }
 
-data "google_iam_policy" "zenotta-mining-iam-policy" {
-  binding {
-    role = "roles/iam.serviceAccountUser"
+# data "google_iam_policy" "zenotta-mining-iam-policy" {
+#   binding {
+#     role = "roles/iam.serviceAccountUser"
 
-    members = [
-      "serviceAccount:{barry.botha@gmail.com}",
-    ]
-  }
-}
+#     members = [
+#       "serviceAccount:{barry.botha@gmail.com}",
+#     ]
+#   }
+# }
 
-resource "google_service_account" "zenotta-mining-service-account" {
-  account_id   = "zenotta-mining-service-account"
-  display_name = "Zenotta Mining Service Account"
-}
+# resource "google_service_account" "zenotta-mining-service-account" {
+#   account_id   = "zenotta-mining-service-account"
+#   display_name = "Zenotta Mining Service Account"
+# }
 
-resource "google_service_account_iam_policy" "admin-account-iam" {
-  service_account_id = google_service_account.zenotta-mining-service-account.name
-  policy_data        = data.google_iam_policy.zenotta-mining-iam-policy.policy_data
-}
+# resource "google_service_account_iam_policy" "admin-account-iam" {
+#   service_account_id = google_service_account.zenotta-mining-service-account.name
+#   policy_data        = data.google_iam_policy.zenotta-mining-iam-policy.policy_data
+# }
