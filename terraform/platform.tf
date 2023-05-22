ebab9ae8945ec64473ccf30b402ebdbfa656dc79
Barry Botha <barry@hydracorp.ltd>
2023-05-22 18:40:44 +0300

    setup new iam

--- terraform/platform.tf
+++ terraform/platform.tf
@@ -1,23 +1,24 @@
 resource "google_project" "zenotta-mining" {
-  name       = "Zenotta Mining"
-  project_id = "zenotta-mining"
+  name       = var.project
+  project_id = var.projectId
   auto_create_network = false
+  billing_account = var.billingAccount
 }
 
-# data "google_iam_policy" "zenotta-mining-iam-policy" {
-#   binding {
-#     role = "roles/iam.serviceAccountUser"
+data "google_iam_policy" "zenotta-mining-iam-policy" {
+  binding {
+    role = "roles/iam.serviceAccountUser"
 
-#     members = [
-#       "serviceAccount:{barry.botha@gmail.com}",
-#     ]
-#   }
-# }
+    members = [
+      google_service_account.zenotta-mining-service-account.email
+    ]
+  }
+}
 
-# resource "google_service_account" "zenotta-mining-service-account" {
-#   account_id   = "zenotta-mining-service-account"
-#   display_name = "Zenotta Mining Service Account"
-# }
+resource "google_service_account" "zenotta-mining-service-account" {
+  account_id   = "zenotta-mining-service-account"
+  display_name = "Zenotta Mining Service Account"
+}
 
 # resource "google_service_account_iam_policy" "admin-account-iam" {
 #   service_account_id = google_service_account.zenotta-mining-service-account.name
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,16 @@ variable "project" {
   default = "zenotta-mining"
 }
 
+variable "projectId" {
+    type = string
+    default = "zenotta-mining-387514"
+}
+
+variable "billingAccount" {
+  type    = string
+  default = "014B21-4E10C8-3EAEB6"
+}
+
 variable "region" {
   type    = string
   default = "us-central1"
