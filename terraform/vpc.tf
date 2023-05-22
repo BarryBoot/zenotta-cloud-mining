cd66d67af9aa0f54aa414139c8efedf01b2ff4d6
Barry Botha <barry@hydracorp.ltd>
2023-05-22 21:07:06 +0300

    fix network create errors

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,5 @@
 resource "google_compute_network" "zenotta-mining-network" {
+  project                 = var.project
   name                    = "zenotta-mining-network"
   auto_create_subnetworks = true
 }
