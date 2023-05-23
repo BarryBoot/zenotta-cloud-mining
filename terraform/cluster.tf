5660a20206e44d2dda41cd79f6a6cf01219fd9fc
Barry Botha <barry@hydracorp.ltd>
2023-05-23 08:43:47 +0300

    destroy cluster

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,52 +1,52 @@
-resource "google_container_cluster" "zenotta-mining-cluster" {
+# resource "google_container_cluster" "zenotta-mining-cluster" {
 
-  name           = "zenotta-mining-cluster"
-  location       = var.location
-  node_locations = var.node_locations
-  project        = var.projectId
+#   name           = "zenotta-mining-cluster"
+#   location       = var.location
+#   node_locations = var.node_locations
+#   project        = var.projectId
 
-  network = google_compute_network.zenotta-mining-network.id
-  #subnetwork = google_compute_subnetwork.zenotta-mining-network.id
+#   network = google_compute_network.zenotta-mining-network.id
+#   #subnetwork = google_compute_subnetwork.zenotta-mining-network.id
 
-  initial_node_count = 1
+#   initial_node_count = 1
 
-  node_config {
+#   node_config {
 
-    preemptible = true
-    #machine_type = "a2-highgpu-1g"
-    machine_type = "g2-standard-16"
+#     preemptible = true
+#     #machine_type = "a2-highgpu-1g"
+#     machine_type = "g2-standard-16"
 
-    guest_accelerator {
-      #type  = "nvidia-tesla-a100"
-      type  = "nvidia-l4"
-      count = 1
-      gpu_sharing_config {
-        gpu_sharing_strategy       = "TIME_SHARING"
-        max_shared_clients_per_gpu = 8
-      }
-    }
+#     guest_accelerator {
+#       #type  = "nvidia-tesla-a100"
+#       type  = "nvidia-l4"
+#       count = 1
+#       gpu_sharing_config {
+#         gpu_sharing_strategy       = "TIME_SHARING"
+#         max_shared_clients_per_gpu = 8
+#       }
+#     }
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.zenotta-node-service-account.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.read_only"
-    ]
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     service_account = google_service_account.zenotta-node-service-account.email
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform",
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#       "https://www.googleapis.com/auth/devstorage.read_only"
+#     ]
 
-  }
+#   }
 
-}
+# }
 
-# Install nVidia drivers
+# # Install nVidia drivers
 
-data "http" "nvidia_driver_installer_manifest" {
-  url = "https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded.yaml"
-}
+# data "http" "nvidia_driver_installer_manifest" {
+#   url = "https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded.yaml"
+# }
 
-resource "kubectl_manifest" "nvidia_driver_installer" {
-  yaml_body = data.http.nvidia_driver_installer_manifest.body
-}
+# resource "kubectl_manifest" "nvidia_driver_installer" {
+#   yaml_body = data.http.nvidia_driver_installer_manifest.body
+# }
 
 
