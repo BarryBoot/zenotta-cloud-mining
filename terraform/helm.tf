ccb011770e249ce5b4905300e9db41f0ee9ad2cb
Barry Botha <barry@hydracorp.ltd>
2023-05-23 21:37:17 +0300

    add helm and nvidia chart

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,3 +1,25 @@
+
+data "google_client_config" "client" {}
+
+data "google_container_cluster" "credentials" {
+  name = google_container_cluster.zenotta-mining-cluster.name
+  location = var.gcp_region
+  project = var.gcp_project
+}
+
+provider "helm" {
+
+  experiments {
+    manifest = true
+  }
+
+  kubernetes {
+    host = data.google_container_cluster.credentials.endpoint
+    token = data.google_client_config.client.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.zenotta-mining-cluster.master_auth.0.cluster_ca_certificate)
+  }
+}
+
 resource "helm_release" "nvidia-drivers" {
   name       = "nvidia-driver"
   chart      = "./helm/nvidia-chart"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,23 +14,3 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-
-  # localhost registry with password protection
-  # registry {
-  #   url      = "oci://localhost:5000"
-  #   username = "username"
-  #   password = "password"
-  # }
-
-  # # private registry
-  # registry {
-  #   url      = "oci://private.registry"
-  #   username = "username"
-  #   password = "password"
-  # }
-}
