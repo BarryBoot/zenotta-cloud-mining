7c89cfee8d9328ebc2ed4a5fa394b6c7549c36dd
Barry Botha <barry@hydracorp.ltd>
2023-05-23 21:50:13 +0300

    add helm and nvidia chart

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -2,9 +2,9 @@
 data "google_client_config" "client" {}
 
 data "google_container_cluster" "credentials" {
-  name = google_container_cluster.zenotta-mining-cluster.name
-  location = var.gcp_region
-  project = var.gcp_project
+  name     = google_container_cluster.zenotta-mining-cluster.name
+  location = var.location
+  project  = var.projectId
 }
 
 provider "helm" {
@@ -14,13 +14,13 @@ provider "helm" {
   }
 
   kubernetes {
-    host = data.google_container_cluster.credentials.endpoint
-    token = data.google_client_config.client.access_token
+    host                   = data.google_container_cluster.credentials.endpoint
+    token                  = data.google_client_config.client.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.zenotta-mining-cluster.master_auth.0.cluster_ca_certificate)
   }
 }
 
 resource "helm_release" "nvidia-drivers" {
-  name       = "nvidia-driver"
-  chart      = "./helm/nvidia-chart"
+  name  = "nvidia-driver"
+  chart = "./helm/nvidia-chart"
 }
