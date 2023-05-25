85ecd71e0205c9c070f7426e026dc6fde26fce4b
Barry Botha <barry@hydracorp.ltd>
2023-05-25 18:16:42 +0300

    try cache in github and deploy miner chart

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -14,8 +14,6 @@ provider "helm" {
 
   kubernetes {
     
-    config_path = "~/.kube/config"
-    
     host                   = data.google_container_cluster.credentials.endpoint
     token                  = data.google_client_config.client.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.zenotta-mining-cluster.master_auth.0.cluster_ca_certificate)
