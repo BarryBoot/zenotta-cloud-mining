bea6f7d68900412d2d245b0edba903118ecc98aa
Barry Botha <barry@hydracorp.ltd>
2023-05-25 15:36:53 +0300

    add helm chart for miner and imge build on gihub actions

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,26 +1,30 @@
 
-data "google_client_config" "client" {}
+# data "google_client_config" "client" {}
 
-data "google_container_cluster" "credentials" {
-  name     = google_container_cluster.zenotta-mining-cluster.name
-  location = var.location
-  project  = var.projectId
-}
+# data "google_container_cluster" "credentials" {
+#   name     = google_container_cluster.zenotta-mining-cluster.name
+#   location = var.location
+#   project  = var.projectId
+# }
 
-provider "helm" {
+# provider "helm" {
 
-  experiments {
-    manifest = true
-  }
+#   experiments {
+#     manifest = true
+#   }
 
-  kubernetes {
-    host                   = data.google_container_cluster.credentials.endpoint
-    token                  = data.google_client_config.client.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.zenotta-mining-cluster.master_auth.0.cluster_ca_certificate)
-  }
-}
+#   kubernetes {
+    
+#     config_path = "~/.kube/config"
+    
+#     host                   = data.google_container_cluster.credentials.endpoint
+#     token                  = data.google_client_config.client.access_token
+#     cluster_ca_certificate = base64decode(google_container_cluster.zenotta-mining-cluster.master_auth.0.cluster_ca_certificate)
+  
+#   }
+# }
 
-resource "helm_release" "nvidia-drivers" {
-  name  = "nvidia-driver"
-  chart = "./helm/nvidia-chart"
-}
+# resource "helm_release" "nvidia-drivers" {
+#   name  = "nvidia-driver"
+#   chart = "./helm/nvidia-chart"
+# }
