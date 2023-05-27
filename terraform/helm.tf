fb07624bdc051cf084ecd949113a01b0ab68cea5
Barry Botha <barry@hydracorp.ltd>
2023-05-27 18:44:14 +0300

    fixing helm state issues

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -16,11 +16,11 @@ provider "helm" {
     host                   = data.google_container_cluster.credentials.endpoint
     token                  = data.google_client_config.client.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.zenotta-mining-cluster.master_auth.0.cluster_ca_certificate)
-    # exec {
-    #   api_version = "client.authentication.k8s.io/v1beta1"
-    #   command     = "gcloud"
-    #   args        = ["container", "get-credentials", google_container_cluster.zenotta-mining-cluster.name, "--region", var.region, "--project", var.projectId]
-    # }
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "gcloud"
+      args        = ["container", "get-credentials", google_container_cluster.zenotta-mining-cluster.name, "--region", var.region, "--project", var.projectId]
+    }
   }
 }
 # gcloud container clusters get-credentials zenotta-mining-cluster --region us-central1 --project zenotta-mining-387514
