2fff0635f94ea3e784fea86a634e45b9ffe9a2c9
Barry Botha <barry@hydracorp.ltd>
2023-06-01 18:55:20 +0300

    10GB regional balanced is cheapest. pods take 3GB. we've got spare

--- terraform/gke-disks/main.tf
+++ terraform/gke-disks/main.tf
@@ -1,23 +1,23 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.65.2"
     }
   }
 }
 
 resource "google_compute_region_disk" "default" {
-  count      = 8
-  name  = "zenotta-miner-disk-${var.nodeIndex}-${count.index}"
-  type  = "pd-standard"
-  region  = var.region
+  count         = 8
+  name          = "zenotta-miner-disk-${var.nodeIndex}-${count.index}"
+  type          = "pd-balanced"
+  region        = var.region
   replica_zones = var.replica_zones
-  
+
   labels = {
     owner = "${var.miners[count.index].owner}"
   }
-  
-  size = 2
+
+  size = 10
 
 }
