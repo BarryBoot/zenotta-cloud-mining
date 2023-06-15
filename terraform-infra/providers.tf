d245bb77c8936dac54c3451c6737d01e7e0c038e
Barry Botha <barry@hydracorp.ltd>
2023-06-15 11:00:30 +0300

    version bump

--- terraform-infra/providers.tf
+++ terraform-infra/providers.tf
@@ -1,11 +1,11 @@
 terraform {
 
-  required_version = ">= 1.4.6"
+  required_version = "1.5"
   
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.65.2"
+      version = "4.69.1"
     }
 
   }
